94f0d490f779bfac2d150d1b3c65975ef1f2870f
Rob Robbins <rob.robb.ns@gmail.com>
2018-07-27 11:49:03 -0700

    implemented registry deploy actions and support

--- src/redux/dispatchers/registry.ts
+++ src/redux/dispatchers/registry.ts
@@ -1,11 +1,22 @@
 import { Nos } from 'computable/dist/types'
 import store from '../store'
-import { apply as getApplicant } from '../action-creators/registry'
-import { TokenDefaults } from '../../constants'
+import {
+  apply as getApplicant,
+  deployRegistry as deploy,
+  resetRegistry as reset,
+} from '../action-creators/registry'
 
+// TODO revisit now that we have some real implementations in. Likely it should be an async promise for a TransactionReceipt like token#approve
 const apply = (name:string, deposit?:Nos, data?:string): void => {
   // TODO default for deposit? App needs/has parameterizer defaults?
   store.dispatch(getApplicant(name, deposit || 0, data)) // this should move an application into the applicants list
 }
 
-export { apply }
+const deployRegistry = async (name:string, address?:string): Promise<string> =>
+  store.dispatch(deploy(name, address))
+
+const resetRegistry = (): void => {
+  store.dispatch(reset())
+}
+
+export { apply, deployRegistry, resetRegistry }
